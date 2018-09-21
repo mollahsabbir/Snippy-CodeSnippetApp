3c7dd2c54bfb3634e7921a6b749bfbcb0916b959
SabbirMollah <mollahsabbir@gmail.com>
2018-09-21 22:34:17 +0600

    Added info to About Page. Added Gitignore

--- app/src/main/java/com/tritekcorp/user/snippy/AboutActivity.java
+++ app/src/main/java/com/tritekcorp/user/snippy/AboutActivity.java
@@ -1,7 +1,12 @@
 package com.tritekcorp.user.snippy;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        TextView aboutInfoTextView = (TextView) findViewById(R.id.aboutInfoTextView);
+
+        String help = "One Tap - See Snippet\nLong Tap - Delete Snippet\n\n";
+        String info = "To see the LICENSE: ";
+
+        aboutInfoTextView.setText(help + info );
+    }
+
+    public void sendToGithubRepo(View view){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://github.com/SabbirMollah/Snippy-CodeSnippetApp.git"));
+        startActivity(intent);
     }
 }
